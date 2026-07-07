0c240ec96aff0beb68490ad08c8bfbfc364de263
Mor4n <anm0ran@outlook.com>
2026-07-07 00:51:38 -0700

    Añadido arreglo de objetos Notification con distintas notificaciones

--- src/tests/Test.java
+++ src/tests/Test.java
@@ -1,6 +1,8 @@
 package tests;
 
+
 import clases.EmailNotification;
+import clases.Notification;
 import clases.PushNotification;
 import clases.SmsNotification;
 
@@ -13,9 +15,22 @@ public class Test {
         // SmsNotification s = new SmsNotification("5483749582", "Soy un sms de un sony ericson<3");
         // System.out.println(s.toString());
 
-        PushNotification p = new PushNotification("a", "Soy una push notification");
+        // PushNotification p = new PushNotification("a", "Soy una push notification");
+        // System.out.println(p.toString());
 
-        System.out.println(p.toString());
+
+        Notification[] notifications = {
+            new EmailNotification("alex@gmail.com", "Recibo de pago", "Aquí tiene su recibo de pago"),
+            new SmsNotification("6221232432", "Registra tu celular antes de la fecha límite"),
+            new EmailNotification("brayan@outlook.com", "Registro exitoso", "Gracias por registrarte en nuestra página"),
+            new SmsNotification("6352313243", "Su saldo se ha agotado"),
+            new SmsNotification("6321495145", "Su tarjeta no cuenta con los fondos disponibles para hacer la compra"),
+            new SmsNotification("6321495145", "Su mensaje no pudo ser enviado con éxito"),
+            new PushNotification("acxwp132", "Has recibido una nueva solicitud de amistad")};
+        
+        
+
+
     }
 
 }
